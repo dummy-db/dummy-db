5dd5ec0ba91aa9a55efb4c95d6772b626855d88b
CasperSocio <casper.tollefsen@outlook.com>
2025-08-12 10:48:33 +0200

    types: add remaining type definitions

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,1 +1,20 @@
 export type DocumentData = { [field: string]: any }
+
+export type DocumentMeta = {
+  createdAt: string
+  updatedAt: string
+}
+
+export type Record<ModelType extends DocumentData> = {
+  data: ModelType
+  id: string
+  meta: DocumentMeta
+}
+
+export type WhereClause<ModelType extends DocumentData> = {
+  field: keyof ModelType
+  op: WhereFilterOp
+  value: any
+}
+
+export type WhereFilterOp = '==' | '!=' | '<' | '<=' | '>' | '>='
